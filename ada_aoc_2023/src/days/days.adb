f1f616d0bf2fab82ecb252c31dcbb703cbf7c7ca
Alex Powell <alexjohnpowell93@gmail.com>
2023-12-02 23:51:56 +0000

    Made it call function

--- ada_aoc_2023/src/days/days.adb
+++ ada_aoc_2023/src/days/days.adb
@@ -7,6 +7,7 @@ package body Days is
     
     procedure Run_Day_1( Input_File: String ) is
     begin
-        Put_Line( "--- Day 1 ---" );
+      Put_Line( "--- Day 1 ---" );
+      Day_1.Part_1(Input_File);
     end Run_Day_1;
 end Days;
